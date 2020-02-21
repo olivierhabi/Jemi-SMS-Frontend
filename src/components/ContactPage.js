1e2553b8509a1013997beadccbdaabf9fbd98a16
Olivier <habimanaolivier6@gmail.com>
2020-02-21 19:37:57 +0200

    Add Contact UI

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFileImport,
+  faExclamationCircle
+} from "@fortawesome/free-solid-svg-icons";
 import API from "./Api";
 import { useHistory } from "react-router-dom";
 import DropZone from "../lib/DropZone";
@@ -72,7 +77,98 @@ const ContactPage = () => {
   };
 
   return (
-    <div>
+    <main class="content">
+      <div class="container-fluid p-0">
+        <h1 id="header-h1" class="h3 mb-3">
+          Contacts
+        </h1>
+        <div id="message-row">
+          <div class="card">
+            <div id="signup-form">
+              <div class="media">
+                <div class="media-body">
+                  <div id="import-content">
+                    <div>
+                      <DropZone
+                        getJson={jsonResult => {
+                          setState({ jsonResult });
+                        }}
+                      >
+                        <p id="import-contact">
+                          <FontAwesomeIcon icon={faFileImport} />
+                          Import Contact.
+                        </p>
+                      </DropZone>
+                    </div>
+                  </div>
+
+                  {state.jsonResult ? (
+                    <div>
+                      <label id="username-label" for="usrename">
+                        Imported Contacts
+                      </label>
+                      <div
+                        id="contacts-forms"
+                        class="container-fluid vh-50 d-flex flex-column"
+                      >
+                        <div
+                          id="locationlist"
+                          class="col overflow-auto bg-info"
+                        >
+                          <div class="position-absolute">
+                            {state.jsonResult.map((contact, index) => {
+                              // eslint-disable-next-line no-redeclare
+                              var index = index + 1;
+                              return (
+                                <div class="form-row">
+                                  <p id="index-number">{index}. </p>
+                                  <div id="username-input">
+                                    <input
+                                      key={index}
+                                      type="text"
+                                      name="phone"
+                                      defaultValue={contact.name}
+                                      class="form-control"
+                                      id="contact-save"
+                                    />
+                                  </div>
+                                  <div id="username-input">
+                                    <input
+                                      key={index}
+                                      type="text"
+                                      name="phone"
+                                      defaultValue={contact.phone}
+                                      class="form-control"
+                                      id="contact-save"
+                                    />
+                                  </div>
+                                </div>
+                              );
+                            })}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ) : null}
+                </div>
+              </div>
+              <form class="needs-validation" onSubmit={importContact}>
+                <div class="form-row">{/*<Error />*/}</div>
+                <button
+                  id="signup-btn"
+                  class="btn btn-outline-light"
+                  type="submit"
+                >
+                  Save
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+  /*<div>
       <div style={{ width: 640, margin: "15px auto" }}>
         <div>
           <DropZone
@@ -122,9 +218,8 @@ const ContactPage = () => {
           />
           <button>Save contact</button>
         </form>
-      </div>
-    </div>
-  );
+                </div>
+    </div>*/
 };
 
 export default ContactPage;
